2b9202bb8c0a5f5a363d3475a04f5fa88ffab5d0
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-02-08 11:44:03 +0700

    Sample #2: insert ke table relasi hasOne

--- database/seeds/Playground.php
+++ database/seeds/Playground.php
@@ -30,5 +30,25 @@ class Playground extends Seeder
         $data = ['name' => 'Bayu Hendra', 'email' => 'bayu.hendra@javan.co.id', 'password' => bcrypt('asdf1234')];
         \App\Models\User::create($data);
 
+        /*
+         * Insert relasi hasOne
+        */
+
+        // Cara 1
+        $data = ['location' => 'Jogja', 'bio' => 'Programmer at Javan', 'user_id' => 1];
+        \Illuminate\Support\Facades\DB::table('user_profiles')->insert($data);
+
+        // Cara 2
+        $profile = new \App\Models\UserProfile();
+        $profile->location = 'Sragen';
+        $profile->bio = 'Anak Petani Kode';
+        $profile->user_id = 2;
+        $profile->save();
+
+        // Cara 3
+        $user = \App\Models\User::first();
+        $profile = \App\Models\UserProfile::make(['location' => 'Bandung', 'bio' => 'Urang Deuy']);
+        $user->profile()->save($profile);
+
     }
 }
